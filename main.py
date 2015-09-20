19ea46a893fb8e45aa639937949c3561facead23
Jerome Leclanche <jerome@leclan.ch>
2015-09-21 00:10:07 +0200

    Use a global pattern for entity strings

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from xml.dom import minidom
 
 __version__ = "0.9"
 
+_E = r"(GameEntity|UNKNOWN HUMAN PLAYER|\[.+\]|\d+|.+)"
 
 POWERLOG_LINE_RE = re.compile(r"^D ([\d:.]+) ([^(]+)\(\) - (.+)$")
 OUTPUTLOG_LINE_RE = re.compile(r"\[Power\] ()([^(]+)\(\) - (.+)$")
@@ -16,27 +17,27 @@ OUTPUTLOG_LINE_RE = re.compile(r"\[Power\] ()([^(]+)\(\) - (.+)$")
 ENTITY_RE = re.compile("\[.*\s*id=(\d+)\s*.*\]")
 
 CHOICES_CHOICE_RE = re.compile(r"id=(\d+) PlayerId=(\d+) ChoiceType=(\w+) CountMin=(\d+) CountMax=(\d+)$")
-CHOICES_SOURCE_RE = re.compile(r"Source=(\[?.+\]?)$")
+CHOICES_SOURCE_RE = re.compile(r"Source=%s$" % _E)
 CHOICES_ENTITIES_RE = re.compile(r"Entities\[(\d+)\]=(\[.+\])$")
 
 SEND_CHOICES_CHOICETYPE_RE = re.compile(r"id=(\d+) ChoiceType=(.+)$")
 SEND_CHOICES_ENTITIES_RE = re.compile(r"m_chosenEntities\[(\d+)\]=(\[.+\])$")
 
 OPTIONS_ENTITY_RE = re.compile(r"id=(\d+)$")
-OPTIONS_OPTION_RE = re.compile(r"option (\d+) type=(\w+) mainEntity=(.*)$")
-OPTIONS_SUBOPTION_RE = re.compile(r"(subOption|target) (\d+) entity=(.*)$")
+OPTIONS_OPTION_RE = re.compile(r"option (\d+) type=(\w+) mainEntity=%s?$" % _E)
+OPTIONS_SUBOPTION_RE = re.compile(r"(subOption|target) (\d+) entity=%s$" % _E)
 
 SEND_OPTION_RE = re.compile(r"selectedOption=(\d+) selectedSubOption=(-1|\d+) selectedTarget=(\d+) selectedPosition=(\d+)")
 
 ACTION_TAG_RE = re.compile(r"tag=(\w+) value=(\w+)")
-ACTION_FULLENTITY_RE_1 = re.compile(r"FULL_ENTITY - Updating (\[.+\]) CardID=(\w+)?$")
+ACTION_FULLENTITY_RE_1 = re.compile(r"FULL_ENTITY - Updating %s CardID=(\w+)?$" % _E)
 ACTION_FULLENTITY_RE_2 = re.compile(r"FULL_ENTITY - Creating ID=(\d+) CardID=(\w+)?$")
-ACTION_SHOWENTITY_RE = re.compile(r"SHOW_ENTITY - Updating Entity=(\[?.+\]?) CardID=(\w+)$")
-ACTION_HIDEENTITY_RE = re.compile(r"HIDE_ENTITY - Entity=(\[?.+\]?) tag=(\w+) value=(\w+)")
-ACTION_TAGCHANGE_RE = re.compile(r"TAG_CHANGE Entity=(\[?.+\]?) tag=(\w+) value=(\w+)")
-ACTION_START_RE = re.compile(r"ACTION_START Entity=(\[?.+\]?) (?:SubType|BlockType)=(\w+) Index=(-1|\d+) Target=(\[?.+\]?)$")
-ACTION_METADATA_RE = re.compile(r"META_DATA - Meta=(\w+) Data=(\[?.+\]?) Info=(\d+)")
-ACTION_METADATA_INFO_RE = re.compile(r"Info\[(\d+)\] = (\[?.+\]?)")
+ACTION_SHOWENTITY_RE = re.compile(r"SHOW_ENTITY - Updating Entity=%s CardID=(\w+)$" % _E)
+ACTION_HIDEENTITY_RE = re.compile(r"HIDE_ENTITY - Entity=%s tag=(\w+) value=(\w+)$" % _E)
+ACTION_TAGCHANGE_RE = re.compile(r"TAG_CHANGE Entity=%s tag=(\w+) value=(\w+)" % _E)
+ACTION_START_RE = re.compile(r"ACTION_START Entity=%s (?:SubType|BlockType)=(\w+) Index=(-1|\d+) Target=%s$" % (_E, _E))
+ACTION_METADATA_RE = re.compile(r"META_DATA - Meta=(\w+) Data=%s Info=(\d+)" % _E)
+ACTION_METADATA_INFO_RE = re.compile(r"Info\[(\d+)\] = %s" % _E)
 ACTION_CREATEGAME_RE = re.compile(r"GameEntity EntityID=(\d+)")
 ACTION_CREATEGAME_PLAYER_RE = re.compile(r"Player EntityID=(\d+) PlayerID=(\d+) GameAccountId=\[hi=(\d+) lo=(\d+)\]$")
 
