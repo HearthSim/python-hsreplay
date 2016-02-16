38d1885207e6f36f0ed2490678ca31f68ffd99b9
Jerome Leclanche <jerome@leclan.ch>
2016-02-17 00:01:05 +0200

    python: Add a --processor argument to switch to PowerTaskList

--- python/convert.py
+++ python/convert.py
@@ -7,10 +7,11 @@ from hsreplay import log_to_xml
 def main():
 	parser = ArgumentParser()
 	parser.add_argument("files", nargs="*")
+	parser.add_argument("--processor", dest="processor", default="GameState")
 	args = parser.parse_args(sys.argv[1:])
 	for filename in args.files:
 		with open(filename) as f:
-			print(log_to_xml(f))
+			print(log_to_xml(f, args.processor))
 
 
 if __name__ == "__main__":
