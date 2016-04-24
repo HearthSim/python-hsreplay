dac818ddd479c451c71f51fc7fe5c9caab34ad33
Tamás Király <info@rokk.hu>
2016-04-24 19:35:17 +0200

    create file with same name in the app's root folder

--- python/convert.py
+++ python/convert.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import sys
+import codecs
 from argparse import ArgumentParser, ArgumentTypeError
 from datetime import datetime
-from hsreplay import log_to_xml
+from hsreplay.dumper import log_to_xml
+from os import path
+from os.path import basename
 
 
 def date_arg(s):
@@ -17,10 +20,13 @@ def main():
 	parser.add_argument("files", nargs="*")
 	parser.add_argument("--processor", dest="processor", default="GameState")
 	parser.add_argument("--default-date", dest="date", type=date_arg, help="Format: YYYY-MM-DD")
+	# https://stackoverflow.com/questions/9226516/
+	sys.stdout = codecs.getwriter("utf-8")(sys.stdout.buffer)
 	args = parser.parse_args(sys.argv[1:])
 	for filename in args.files:
-		with open(filename) as f:
-			print(log_to_xml(f, args.processor, args.date))
+		with open(filename, encoding="utf-8") as f:
+			out = open(path.splitext(basename(filename))[0] + ".hsreplay",'w', encoding="utf-8")
+			print(log_to_xml(f, args.processor, args.date),file=out)
 
 
 if __name__ == "__main__":
