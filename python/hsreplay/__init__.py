d64b1a0c06683151b1037bd8cb1f1644ceb30f70
Jerome Leclanche <jerome@leclan.ch>
2016-05-21 00:05:20 +0300

    Release HSReplay 1.2

--- python/hsreplay/__init__.py
+++ python/hsreplay/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = "Jerome Leclanche"
 __email__ = "jerome@leclan.ch"
-__version__ = "1.1"
+__version__ = "1.2"
 
 SYSTEM_DTD = "https://hearthsim.info/hsreplay/dtd/hsreplay-%s.dtd" % (__version__)
