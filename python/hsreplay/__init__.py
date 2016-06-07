6b1b1f99958f81fe36d9cfd06bd08c44131660ef
Jerome Leclanche <jerome@leclan.ch>
2016-06-07 03:06:12 +0300

    python: Separate __version__ into DTD_VERSION and LIB_VERSION

--- python/hsreplay/__init__.py
+++ python/hsreplay/__init__.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
+DTD_VERSION = "1.2"
+LIB_VERSION = "0"
 __author__ = "Jerome Leclanche"
 __email__ = "jerome@leclan.ch"
-__version__ = "1.2"
+__version__ = "%s.s" % (DTD_VERSION, LIB_VERSION)
+
 
 SYSTEM_DTD = "https://hearthsim.info/hsreplay/dtd/hsreplay-%s.dtd" % (__version__)
