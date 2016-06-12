b9335751bc51e0c52d682540e5fb3db89839ac1b
JeffCarpenter <carpenter.jeff@gmail.com>
2016-06-11 18:43:18 -0700

    Fix format string of __version__ in __init__.py

--- python/hsreplay/__init__.py
+++ python/hsreplay/__init__.py
@@ -4,7 +4,7 @@ DTD_VERSION = "1.2"
 LIB_VERSION = "0"
 __author__ = "Jerome Leclanche"
 __email__ = "jerome@leclan.ch"
-__version__ = "%s.s" % (DTD_VERSION, LIB_VERSION)
+__version__ = "%s.%s" % (DTD_VERSION, LIB_VERSION)
 
 
 SYSTEM_DTD = "https://hearthsim.info/hsreplay/dtd/hsreplay-%s.dtd" % (__version__)
