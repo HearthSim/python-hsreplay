2703306225298fbd9d3ed4f33f27d9b302889f88
Jerome Leclanche <jerome@leclan.ch>
2016-07-10 11:09:17 +0300

    Release 1.3.5

--- hsreplay/__init__.py
+++ hsreplay/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 DTD_VERSION = "1.3"
-LIB_VERSION = 4
+LIB_VERSION = 5
 __author__ = "Jerome Leclanche"
 __email__ = "jerome@leclan.ch"
 __version__ = "%s.%s" % (DTD_VERSION, LIB_VERSION)
