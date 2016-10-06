9b81f760615eb857a3908ad02b9bea77fb70b3d6
Jerome Leclanche <jerome@leclan.ch>
2016-10-06 09:33:57 +0300

    Update maintainer email

--- python/hsreplay/__init__.py
+++ python/hsreplay/__init__.py
@@ -3,7 +3,7 @@
 DTD_VERSION = "1.3"
 LIB_VERSION = 6
 __author__ = "Jerome Leclanche"
-__email__ = "jerome@leclan.ch"
+__email__ = "jerome@hearthsim.net"
 __version__ = "%s.%s" % (DTD_VERSION, LIB_VERSION)
 
 
