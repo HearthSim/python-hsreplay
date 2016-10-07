a3b928eae0e4fd4740375c5dd1e545ed09e48853
Jerome Leclanche <jerome@leclan.ch>
2016-10-08 01:08:04 +0300

    Add some build assertions in lossless tests

--- test_lossless_loading.py
+++ test_lossless_loading.py
@@ -6,6 +6,9 @@ from io import BytesIO
 from hsreplay.document import HSReplayDocument
 
 
+BUILD = 12345
+
+
 def main():
 	parser = ArgumentParser()
 	parser.add_argument("files", nargs="*")
@@ -13,11 +16,12 @@ def main():
 	default_date = datetime.now()
 	for filename in args.files:
 		with open(filename) as f:
-			doc_in = HSReplayDocument.from_log_file(f, date=default_date)
+			doc_in = HSReplayDocument.from_log_file(f, date=default_date, build=BUILD)
 			xml_in = doc_in.to_xml(pretty=True)
 			xml_file_in = BytesIO(xml_in.encode("utf-8"))
 			doc_out = HSReplayDocument.from_xml_file(xml_file_in)
 			xml_out = doc_out.to_xml(pretty=True)
+			assert 'build="%i"' % (BUILD) in xml_out, "Can't find build in output file!"
 
 			if xml_in != xml_out:
 				with open("in.xml", "w") as f, open("out.xml", "w") as f2:
@@ -28,7 +32,7 @@ def main():
 				print("%r: Log -> XML -> Document -> XML: SUCCESS" % (filename))
 
 			packet_tree_in = doc_in.to_packet_tree()
-			doc_out2 = HSReplayDocument.from_packet_tree(packet_tree_in)
+			doc_out2 = HSReplayDocument.from_packet_tree(packet_tree_in, build=BUILD)
 			xml_out2 = doc_out2.to_xml(pretty=True)
 
 			if xml_in != xml_out2:
