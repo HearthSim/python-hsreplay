7087cb30cb8e05df5f7b331cb284e05320e85786
Jerome Leclanche <jerome@leclan.ch>
2017-03-08 03:23:30 +0200

    Move metadata to setup.cfg

--- hsreplay/__init__.py
+++ hsreplay/__init__.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python
+import pkg_resources
+
+
+__version__ = pkg_resources.require("hsreplay")[0].version
 
 DTD_VERSION = "1.3"
-LIB_VERSION = 9
-__author__ = "Jerome Leclanche"
-__email__ = "jerome@hearthsim.net"
-__version__ = "%s.%s" % (DTD_VERSION, LIB_VERSION)
-
-
 SYSTEM_DTD = "https://hearthsim.info/hsreplay/dtd/hsreplay-%s.dtd" % (DTD_VERSION)
