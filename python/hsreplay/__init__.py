3c5b9d50d570a1cb1208e3e9c01714c2f9839243
Jerome Leclanche <jerome@leclan.ch>
2018-03-12 20:45:20 +0200

    Release HSReplay 1.6

--- python/hsreplay/__init__.py
+++ python/hsreplay/__init__.py
@@ -3,5 +3,5 @@ import pkg_resources
 
 __version__ = pkg_resources.require("hsreplay")[0].version
 
-DTD_VERSION = "1.5"
+DTD_VERSION = "1.6"
 SYSTEM_DTD = "https://hearthsim.info/hsreplay/dtd/hsreplay-%s.dtd" % (DTD_VERSION)
