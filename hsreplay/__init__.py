59ad7d3c45d73777859ac28059d06b6d7c43e925
Jerome Leclanche <jerome@leclan.ch>
2018-05-17 11:00:35 +0300

    Update DTD version to 1.7

--- hsreplay/__init__.py
+++ hsreplay/__init__.py
@@ -3,5 +3,5 @@ import pkg_resources
 
 __version__ = pkg_resources.require("hsreplay")[0].version
 
-DTD_VERSION = "1.6"
+DTD_VERSION = "1.7"
 SYSTEM_DTD = "https://hearthsim.info/hsreplay/dtd/hsreplay-%s.dtd" % (DTD_VERSION)
